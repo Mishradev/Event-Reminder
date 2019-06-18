b1407b1fa44fb7fd5aea2f929cea2762b2febdff
Nitish Mishra <48508452+Mishradev@users.noreply.github.com>
2019-06-18 23:25:45 +0530

    Add files via upload

---
+++ EvenDer/Source/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  EventMe
+//
+//  Created by Nitish Mishra on 18 May, 2018.
+//  Copyright © 2017 Nitish Mishra. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    // MARK: - UIApplicationDelegate
+    
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        return true
+    }
+}
